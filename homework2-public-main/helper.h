350c10bf02a984f70d2bccf6c69ac4ab9cf5a747
DariusIuga <dariusiuga2@gmail.com>
2024-05-03 00:08:28 +0300

    Moved macros out of helper

---
+++ homework2-public-main/helper.h
@@ -0,0 +1,22 @@
+#ifndef HELPERS_H
+#define _HELPERS_H 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Macro de verificare a erorilor
+ * Exemplu:
+ *     int fd = open(file_name, O_RDONLY);
+ *     DIE(fd == -1, "open failed");
+ */
+
+#define DIE(assertion, call_description)    \
+    if (assertion) {                    \
+        fprintf(stderr, "(%s, %d): ",    \
+                __FILE__, __LINE__);    \
+        perror(call_description);        \
+        exit(EXIT_FAILURE);                \
+    }                                    \
+
+#endif
